3ad2f77a5a1a9f0e98cfac8c11ef3fec7b4a1fad
Yen Cao <yencao@MacBook-Air-von-Yen.local>
2022-11-09 10:33:36 +0100

    init

---
+++ src/main/java/htwberlin/de/planyourtodo/PlanYourTodoApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.de.planyourtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanYourTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanYourTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwberlin/de/planyourtodo/PlanYourTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.de.planyourtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanYourTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
